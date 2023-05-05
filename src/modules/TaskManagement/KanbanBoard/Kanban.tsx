4d7cb5808d8a26ae17c9ec4b2a11e30284743ba1
reiikennedy <mutuakennedy81@gmail.com>
2023-05-05 17:24:40 +0300

    add tasks to kanban page

--- src/modules/TaskManagement/KanbanBoard/Kanban.tsx
+++ src/modules/TaskManagement/KanbanBoard/Kanban.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import utilsStyles from "@/styles/utils.module.css";
+import styles from "@/modules/TaskManagement/KanbanBoard/styles/Kanban.module.css"
+import { Icon } from "@iconify/react";
+import { TaskComponent } from "@/common/components/Task";
+
 
 const Kanban: React.FC = () => {
+
     return(
         <>
             <h2 className={utilsStyles.headingSection}>Your Board</h2>
+            <div className={styles.Backlog}>
+                <h5 className={utilsStyles.headingMd}>Backlog</h5>
+                <div className={styles.BacklogList}>
+                    <TaskComponent/>
+                </div>
+                
+            </div>
         </>
     )
 }
