ba264669fef7a7c9c5bf80b75cfac7c9aa29630c
reiikennedy <mutuakennedy81@gmail.com>
2023-05-05 17:25:12 +0300

    add tasks to tasks page

--- src/pages/workspace/tasks.tsx
+++ src/pages/workspace/tasks.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import Head from "next/head";
 import { Layout } from "@/common/components/Layout";
 import utilsStyles from "@/styles/utils.module.css";
+import {TaskComponent} from "@/common/components/Task";
 
-interface tasks {
-    name: string,
-    status: "backlog" | "toDo" | "inProgress" | "done",
-    order: number
-}
 
-const Tasks: React.FC<tasks> = ({props} = {name:"Example Task",status:"backlog",order:1})=> {
+const Tasks: React.FC = ()=> {
     return(
         <>
         <Head>My tasks</Head>
-        <Layout><h2 className={utilsStyles.headingSection}>My Tasks</h2></Layout>
+        <Layout>
+            <h2 className={utilsStyles.headingSection}>My Tasks</h2>
+            <TaskComponent/>            
+        </Layout>
         </>
     )
 }
