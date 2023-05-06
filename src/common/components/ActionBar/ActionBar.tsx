a0903017e175d79a44fb971e5a11978ee29b8dfe
reiikennedy <mutuakennedy81@gmail.com>
2023-05-06 12:41:45 +0300

    add delete task button

--- src/common/components/ActionBar/ActionBar.tsx
+++ src/common/components/ActionBar/ActionBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "@/common/components/ActionBar/components/SearchBar";
 import CreateTask from "./components/CreateTask";
+import DeleteTasks from "./components/DeleteTasks";
 import styles from "@/common/components/ActionBar/styles/ActionBar.module.css";
 
 
@@ -8,7 +9,10 @@ const ActionBar: React.FC = () => {
     return(
         <div className={styles.actionBar}>
             <SearchBar/>
-            <CreateTask/>
+            <div className={styles.right}>
+                <CreateTask/>
+                <DeleteTasks/>
+            </div>
         </div>
     )
 }
